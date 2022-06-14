eb3c48a67d5d779d66c76f51b00ca4601b92b4cb
igor.gulyaschy <gulyaschy@gmail.com>
2022-06-15 00:28:58 +0300

    alo

---
+++ Jenkinsfile
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+    stages {
+        stage('Preparing for build') {
+            steps {
+                script {
+                    def isContainerExists = sh(script: "docker-compose ps -q -f name='bot1_bots_1-bots'", returnStdout: true) != ""
+                    def isImageExists = sh(script: "docker-compose images -q bot1_bots_1", returnStdout: true) != ""
+
+                    echo "${isContainerExists}"
+
+                    sh 'docker-compose ps -a'
+
+                    sh 'docker-compose ps'
+
+                    echo "${isImageExists}"
+
+                    if (isContainerExists) {
+                        sh 'docker-compose stop -t 0 bot1_bots_1'
+                        sh 'docker-compose rm bot1_bots_1'
+                    }
+
+                    if (isImageExists) {
+                        sh 'docker-compose rmi -f bot1_bots_1'
+                    }
+                }
+            }
+        }
+        stage('Build Docker Image') {
+        steps {
+                sh 'docker-compose build -t bot1_bots_1 .'
+            }
+        }
+        stage ('Start Application in Docker') {
+            steps {
+                sh 'docker-compose run -p 8083:8080 --name bot1_bots_1 bot1_bots_1'
+            }
+        }
+    }
+}
---
+++ bot.js
@@ -0,0 +1,49 @@
+const TelegramApi = require('node-telegram-bot-api')
+const amqplib = require('amqplib')
+require('dotenv').config()
+
+const bot1 = new TelegramApi(process.env.TELEGRAM_BOT1, { polling: true })
+
+const bot2 = new TelegramApi(process.env.TELEGRAM_BOT2, { polling: true })
+
+// const connectToRabbit = async  () => { 
+//   const queue = 'queue'
+//   const conn = await amqplib.connect(process.env.AMQP_URL)
+//   const sender = await conn.createChannel()
+//   const receiver = await conn.createChannel()
+//   await receiver.assertQueue(queue)
+//   return { sender, receiver }
+// }
+
+bot1.onText(/\Стартуем/, async (msg) => {
+  const chatId = msg.chat.id
+  const text = msg.text
+  bot1.sendPhoto(chatId, 'http://risovach.ru/upload/2013/12/mem/natalya-morskaya-pehota_38147160_big_.jpeg')
+  console.log(text, msg.chat)
+  const { sender, receiver } = await connectToRabbit()
+  sender.sendToQueue('queue', Buffer.from(String(chatId)))
+  receiver.consume('queue', async (msg) => {
+    if (msg !== null) {
+      console.log('Recieved:', msg.content.toString());
+      bot2.sendPhoto(msg.content.toString(), 'https://static3.tgstat.ru/channels/_0/f2/f2bfb93b7b7df8f8c16ac1ceaabe428d.jpg')
+      receiver.ack(msg);
+    } else {
+      console.log('Consumer cancelled by server');
+    }
+  })
+  setInterval(() => {
+    receiver.consume('queue', async (msg) => {
+      if (msg !== null) {
+        console.log('Recieved:', msg.content.toString());
+        bot2.sendPhoto(msg.content.toString(), 'https://static3.tgstat.ru/channels/_0/f2/f2bfb93b7b7df8f8c16ac1ceaabe428d.jpg')
+        receiver.ack(msg);
+      } else {
+        console.log('Consumer cancelled by server');
+      }
+    })
+  }, 1000*60*15) //15min
+})
+
+// setInterval(() => {
+//   bot1.sendPhoto(766729939, 'https://static3.tgstat.ru/channels/_0/f2/f2bfb93b7b7df8f8c16ac1ceaabe428d.jpg')
+// }, 500)
